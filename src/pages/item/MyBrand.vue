80d045d8cfc1af5253df71037f14160952f229d3
chenjingui <cjg4764071>
2020-08-28 11:38:02 +0800

    Initial commit

---
+++ src/pages/item/MyBrand.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>
+    hello
+  </span>
+</template>
+
+<script>
+  export default {
+    name: "myBrand"
+  }
+</script>
+
+<!-- scoped:当前样式只作用于当前组件的节点 -->
+<style scoped>
+
+</style>
